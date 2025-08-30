f7db0aa274c6c9a5f9fa9acf030efcd0baf6f4cf
Aditya <vasishtaditya8@gmail.com>
2025-08-30 17:22:53 +0530

    cknksd

---
+++ file.js
@@ -0,0 +1,4 @@
+const http = require('http');
+const muserver = http.createServer((req,res)=>{
+    res.writeHead(200, )
+})
--- hello.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = 8000;
-app.listen(PORT, ()=> console.log(`server started at port: ${PORT}`))
-    
-
